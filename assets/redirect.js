f982b03fe051f71fb7b2b8b0e5bf147d725e48fa
Alexis Garado <alexisgarado@gmail.com>
2023-04-20 20:21:03 -0700

    test auto rdirect to cutom url

--- assets/redirect.js
+++
@@ -1,6 +0,0 @@
-//auto redirect from current *.gitlab.io to custom domain
-//send destination domain as domainTo (FQDN)
-function _notGitLab(domainTo=true){
-  var x = location.hostname.search("gitlab.io")!= -1 ? true : false; // true if *.gitlab.io
-  if(x) location.href = domainTo; 
-}
